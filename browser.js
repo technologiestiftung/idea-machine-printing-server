a7618a936250ddac0fb9fa60e3c6e7075f2f2216
Raphael Arce <raphael.arce@ts.berlin>
2023-08-03 16:51:54 +0200

    changed chromium command to opened multiple tabs to bypass credentials issue
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 require('dotenv').config();
 
-exec(`chromium-browser https://${process.env.HTTP_USER}:${process.env.HTTTP_SECRET}@${process.env.SERVER} --start-fullscreen`,
+exec(`chromium-browser https://${process.env.HTTP_USER}:${process.env.HTTTP_SECRET}@${process.env.SERVER} ${process.env.APP_ORIGIN} --start-fullscreen`,
     (error, stdout, stderr) => {
         if (error) {
             console.error(error);
