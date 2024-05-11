c44a4cb878128e9025a11c0e7daa774785cddad6
Raphael Arce <raphael.arce@ts.berlin>
2024-05-11 19:41:58 +0200

    feat: [wip] prettier
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 export default {
-  apps : [
-  {
-    name: "idea-machine-backend",
-    script: "index.js",
-    args: "--env-file=.env",
-  }]
-}
+	apps: [
+		{
+			name: "idea-machine-backend",
+			script: "index.js",
+			args: "--env-file=.env",
+		},
+	],
+};
--- services/bluetooth/bluetooth.js
+++ services/bluetooth/bluetooth.js
@@ -1,6 +1,6 @@
-import { promisify } from 'node:util';
-import { spawn, exec as execCallback } from 'child_process';
-import { setDiceSide, getDices } from '../state/state.js';
+import { promisify } from "node:util";
+import { spawn, exec as execCallback } from "child_process";
+import { setDiceSide, getDices } from "../state/state.js";
 const exec = promisify(execCallback);
 
 /**
@@ -11,25 +11,23 @@ const exec = promisify(execCallback);
  * <?>
  */
 
-
 // todo get mac-address and binding number from env
-const { stdout, stderr } = await exec('sudo rfcomm bind 1 94:B9:7E:FE:F3:96');
-console.log('bind', { stdout, stderr })
+const { stdout, stderr } = await exec("sudo rfcomm bind 1 94:B9:7E:FE:F3:96");
+console.log("bind", { stdout, stderr });
 
-process.on('SIGINT', async () => {});
-process.on('SIGTERM', () => {});
+process.on("SIGINT", async () => {});
+process.on("SIGTERM", () => {});
 
 // todo get binding number from env
-const picocom = spawn('picocom', ['/dev/rfcomm1', '-b', '115200']);
-picocom.stdout.on('data', (data) => {
+const picocom = spawn("picocom", ["/dev/rfcomm1", "-b", "115200"]);
+picocom.stdout.on("data", (data) => {
 	setDiceSide(data);
 	console.log(getDices());
 });
-picocom.stderr.on('data', (data) => console.error(`stderr: ${data}`));
-
+picocom.stderr.on("data", (data) => console.error(`stderr: ${data}`));
 
 // todo get binding number from env
-picocom.on('exit', async () => {
-	const { stdout, stderr } = await exec('sudo rfcomm release rfcomm1');
-	console.log('release',{ stdout, stderr })
+picocom.on("exit", async () => {
+	const { stdout, stderr } = await exec("sudo rfcomm release rfcomm1");
+	console.log("release", { stdout, stderr });
 });
