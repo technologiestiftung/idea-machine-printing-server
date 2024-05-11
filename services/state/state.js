31d3b3b20dfab88eb7db368f56c9facc1d029475
Raphael Arce <raphael.arce@ts.berlin>
2024-05-11 19:39:58 +0200

    BREAKING CHANGE: [wip] v2
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ services/state/state.js
@@ -0,0 +1,34 @@
+const sides = [
+	"A1", "A2", "A3", "A4", "A5", "A6",
+	"B1", "B2", "B3", "B4", "B5", "B6",
+	"C1", "C2", "C3", "C4", "C5", "C6",
+]
+
+const dices = {
+	A: "1",
+	B: "1",
+	C: "1",
+}
+
+function parseDiceSide(input) {
+	return sides.filter(side => input.includes(side))[0];
+}
+
+function setDiceSide(input) {
+	const diceSide = parseDiceSide(input);
+
+	if (!diceSide) {
+		return;
+	}
+
+	const dice = diceSide[0];
+	const side = diceSide[1];
+
+	dices[dice] = side;
+}
+
+function getDices() {
+	return dices;
+}
+
+export { getDices, setDiceSide }
