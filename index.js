3d19149d4524051242d4a1b606cb6ef212a76334
Raphael Arce <raphael.arce@ts.berlin>
2024-05-16 10:27:48 +0200

    feat: [wip] add multiple bluetooth devices and other improvements
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// import "./services/bluetooth/bluetooth.js";
+import bluetoothSerialMonitors from "./services/bluetooth/bluetooth.js";
 import "./services/browser/browser.js";
-import "./services/rest/rest.js";
+import server from "./services/rest/rest.js";
 import "./services/state/state.js";
+
+process.on("SIGINT", () => {
+	bluetoothSerialMonitors.forEach((monitor) => monitor.kill("SIGINT"));
+	server.close();
+});
+
+process.on("SIGTERM", () => {
+	bluetoothSerialMonitors.forEach((monitor) => monitor.kill("SIGTERM"));
+	server.close();
+});
