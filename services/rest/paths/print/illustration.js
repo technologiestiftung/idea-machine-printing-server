224f0ef6d2b00c063dacc3869aef110308a386b9
aeschi <anna.eschenbacher@icloud.com>
2024-05-24 16:17:56 +0200

    feat: adjust prompts WIP

--- services/rest/paths/print/illustration.js
+++ services/rest/paths/print/illustration.js
@@ -1,4 +1,5 @@
 export async function getIllustration({ idea }) {
+	console.log(idea);
 	const response = await fetch("https://api.openai.com/v1/images/generations", {
 		method: "POST",
 		headers: {
@@ -8,10 +9,9 @@ export async function getIllustration({ idea }) {
 		body: JSON.stringify({
 			model: "dall-e-3",
 			prompt: `
-			Generiere ein Bild das folgende als Idee als Illustration darstellt: ${idea}.
-			Die Illustration zeigt einen medium bis wide Shot der Stadtszene.
-			Die Stimmung is positiv. Menschen sollen im Mittelpunkt stehen.
-			Solarpunk, Retro-Futurism, Concept Art.
+			An illustration of ${idea.summary}.
+			The mode of the illustration is	playful, whimsical, energetic, hazy,
+			The style is a mix of SOLARPUNK, and studio Ghibli anime.
 	`,
 			n: 1,
 			size: "1024x1024",
