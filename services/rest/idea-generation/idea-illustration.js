082607517f4d48b5b181b3e6523f387f52948396
Raphael Arce <raphael.arce@ts.berlin>
2024-06-05 11:20:00 +0200

    feat: add pregeneration
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ services/rest/idea-generation/idea-illustration.js
@@ -0,0 +1,47 @@
+import { ILLUSTRATION_FILE_PATH } from "./constants.js";
+import fs from "node:fs";
+
+/**
+ * Download an image from the given URL and save it to the illustration file path.
+ * @param {string} url
+ * @returns {Promise<void>}
+ */
+async function download(url) {
+	const response = await fetch(url);
+	const buffer = await response.arrayBuffer();
+	fs.writeFileSync(ILLUSTRATION_FILE_PATH, Buffer.from(buffer));
+}
+
+/**
+ * Get an illustration for the idea via dall-e 3 and download it.
+ * @param {string} imagePrompt
+ * @returns {Promise<void>}
+ */
+export async function createIllustration(imagePrompt) {
+	const response = await fetch("https://api.openai.com/v1/images/generations", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+		},
+		body: JSON.stringify({
+			model: "dall-e-3",
+			prompt: `
+			Visualize this idea: ${imagePrompt}
+
+			The mood of the illustration is	joyful and futuristic.
+			The style is a mix of SOLARPUNK aesthetic and studio ghibli films.
+			The colors are soft and similar to the movie spirited away.
+			DIGITAL CONCEPT ART, Semi-photorealistic.
+			The scene is illuminated by soft, warm light, creating a utopian atmosphere.
+	`,
+			n: 1,
+			size: "1024x1024",
+		}),
+	});
+
+	const dalleResponse = await response.json();
+	const imgUrl = dalleResponse.data[0].url;
+
+	await download(imgUrl);
+}
--- services/rest/paths/default.js
+++ services/rest/paths/default.js
@@ -1,3 +1,7 @@
+/**
+ * Default path for the REST API.
+ * @param {ServerResponse} response
+ */
 export function handleIsApiAlive(response) {
 	response.end(JSON.stringify({ message: "ok" }));
 }
