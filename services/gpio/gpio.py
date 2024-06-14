7572fadf251c20a09b9e4a667b0ca623606d585d
Raphael Arce <raphael.arce@ts.berlin>
2024-06-14 13:21:25 +0200

    feat: add gpio
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ services/gpio/gpio.py
@@ -0,0 +1,15 @@
+import requests
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+ 
+while True:
+  if GPIO.input(7) == GPIO.HIGH:
+    # print("Pin 7 is HIGH!")
+    requests.get('http://localhost:8000')
+  # elif GPIO.input(7) == GPIO.LOW:
+    # print("Pin 7 is LOW...")
+  sleep(0.05)
