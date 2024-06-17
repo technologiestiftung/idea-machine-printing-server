756e64349f1c13f338161c57c480d3bd02929a46
Raphael Arce <raphael.arce@ts.berlin>
2024-06-17 10:07:09 +0200

    feat: use correct endpoint
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- services/gpio/gpio.py
+++ services/gpio/gpio.py
@@ -9,7 +9,7 @@ GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
   if GPIO.input(7) == GPIO.HIGH:
     # print("Pin 7 is HIGH!")
-    requests.get('http://localhost:8000')
+    requests.get('http://localhost:8000/pick-idea')
   # elif GPIO.input(7) == GPIO.LOW:
     # print("Pin 7 is LOW...")
   sleep(0.05)
