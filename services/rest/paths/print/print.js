d922f66b1082b27115fda4e4eabf22bc42492e63
Raphael Arce <raphael.arce@ts.berlin>
2024-06-19 16:22:52 +0200

    feat: update printing command
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- services/rest/paths/print/print.js
+++ services/rest/paths/print/print.js
@@ -14,13 +14,20 @@ export async function handlePrinting({ body, response }) {
 	const requestBody = Buffer.concat(body).toString();
 	const { postcard_url } = JSON.parse(requestBody);
 
-	console.time("download-postcard");
-	await downloadPostcard(postcard_url);
-	console.timeEnd("download-postcard");
+	try {
+		console.time("download-postcard");
+		await downloadPostcard(postcard_url);
+		console.timeEnd("download-postcard");
 
-	console.time("print-postcard");
-	await print();
-	console.timeEnd("print-postcard");
+		console.time("print-postcard");
+		await print();
+		console.timeEnd("print-postcard");
+	} catch (error) {
+		console.error(error);
+		response.statusCode = 500;
+		response.end(JSON.stringify({ message: `error printing postcard` }));
+		return;
+	}
 
 	response.statusCode = 200;
 	response.end(JSON.stringify({ message: `printing job sent` }));
@@ -36,6 +43,10 @@ async function downloadPostcard(postcard_url) {
 		.from("postcards")
 		.download(postcard_url);
 
+	if (error) {
+		throw error;
+	}
+
 	const buffer = Buffer.from(await data.arrayBuffer());
 
 	fs.writeFileSync(PDF_FILE_PATH, buffer);
@@ -50,8 +61,8 @@ async function print() {
 
 	if (process.env.PRINT === "1") {
 		childProcess.execSync(
-			`lp -o Pagesize=A6 -o landscape -o fit-to-page -o Duplex=DuplexTumble -d ${process.env.PRINTER_NAME} ${PDF_FILE_PATH}`,
-			// `lp -o Pagesize=A6 -o landscape -o fit-to-page -o Duplex=DuplexTumble -d "EPSON XP-8700 Series" ./services/rest/paths/print/postcard.pdf`,
+			`lp -o Pagesize=A6 -o portrait -o fit-to-page -o media=Lower -d ${process.env.PRINTER_NAME} ${PDF_FILE_PATH}`,
+			// `lp -o Pagesize=A6 -o portrait -o fit-to-page -o media=Lower -d "EPSON_XP_8700_Series_USB_" ./services/rest/paths/print/postcard.pdf`,
 		);
 	}
 }
